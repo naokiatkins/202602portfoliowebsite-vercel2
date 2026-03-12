c1cf909a86ac74a231d16f78b987e1123fc5765a
naokishami <naokishami@gmail.com>
2026-03-12 23:17:24 +0900

    revert analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useCallback } from "react";
-import { Analytics } from "@vercel/analytics/next";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import WorkSection from "@/components/WorkSection";
